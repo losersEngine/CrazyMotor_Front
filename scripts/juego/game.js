effb493fb573293242fa218c8a0cd90765064669
Laura <lauritaninetyseven@gmail.com>
2018-11-04 22:58:34 +0100

    Código refinado

--- scripts/juego/game.js
+++ scripts/juego/game.js
@@ -3,31 +3,6 @@ var name;
 var scene;
 var width, height;
 
-function pedirNombre() //solicitar nombre de usuario
-{
-
-    do{
-    
-        name=prompt("Inserta tu nombre","Nombre"); //Nombre es el valor por defecto que tiene la variable name
-        
-	}while(name == "Nombre" || name == "null"); //mientras no escriba nada o lo deje en blanco se le sigue solicitando
-												//pues el nombre es imprescindible para jugar
-
-
-	var newSnake = {
-						
-		funcion: "crearSerpiente", //metodo que se llama desde el handleText
-		params: [name]
-	
-	}
-	game.socket.send(JSON.stringify(newSnake)); //envio de mensaje al socket
-
-
-}
-
-let enPartida = false; //indica si el jugador esta en partida (true) o en el lobby (false)
-let salaP = null; //nombre de la sala donde se encuentra el usuario
-
 class Racer {
 
 	constructor() {
@@ -36,7 +11,6 @@ class Racer {
 		this.velocity = 0.5;
         this.position = [];
 		this.name = null;
-		this.sprite = null;
 		this.sprites = [];
 		this.points = 0;
 
@@ -50,31 +24,6 @@ class Racer {
 		context.drawImage(this.sprites[this.animation],this.position[0], this.position[1],110.4,205.2);
 		
 	}
-
-}
-
-function salir(){ //metodo para salir de una sala (este o no jugando)
-
-	
-	if(game.nextFrame != null) //si no se ha parado el juego, se para
-		game.stopGameLoop();
-	else //si el juego esta parado (esta en la sala esperando a entrar a jugar) se limpia el canvas
-		//game.context.clearRect(0, 0, 900, 540);
-
-	var o = {
-
-		funcion: "salirSala", //funcion en Java
-		params: []
-
-	}
-	game.socket.send(JSON.stringify(o));
-	
-	borrarDiv('#salaActual'); //borramos el div que contiene los nombres de los jugadores en la sala
-	document.getElementById("partidas-container").style.display = 'inline-block';
-	document.getElementById("serpiente").style.display = "block";
-	document.getElementById("ranking").style.display = "inline-block";
-	salaP = null; //lo ponemos a null porque al salir de la sala, ya no se encuentra en esa partida
-	partidas(); //hacemos get de las partidas que hay creadas (APIRest)
 
 }
 
@@ -189,7 +138,6 @@ class Game {
 		this.addRacer(0,"sprite1","jugador1",0,pos,[1,2,3,4,5]);
 		this.racers[0].sprites[this.racers[0].sprites.length-1].onload = function(){ //hasta que no se carga la ultima animacion, no se empieza el gameloop
 
-			that.context.drawImage(that.racers[0].sprite, that.racers[0].position[0], that.racers[0].position[1], 110.4,205.2);
 			that.startGameLoop(); //EL GAMELOOP DEBERIA INICIARSE CUANDO NOS LO INDIQUE BACK
 
 		}
@@ -273,8 +221,7 @@ class Game {
 
 	addRacer(id, sprite,name,ptos, pos, sprites) {
 		this.racers[id] = new Racer();
-		this.racers[id].sprite = new Image();
-		this.racers[id].sprite.src= "../../resources/SPRITES/" + sprite + "/" + sprite + ".1.png";
+
 		for(var i = 0; i < sprites.length; i++){
 
 			this.racers[id].sprites[i] = new Image();
@@ -286,12 +233,6 @@ class Game {
 		this.racers[id].points = ptos;
 	}
 
-	drawImage(){
-
-		this.context.drawImage(this.racers[0].sprite, 100, 100, 80,38);
-		
-
-	}
 	updateRacer(id, position) {
 		if (this.racers[id]) {
 			this.racers[id].position = position;
@@ -332,62 +273,6 @@ class Game {
 		this.context.fillText(this.racers[id].nombre + ": " + this.racers[id].points,19,space); //space es la posicion en y de las letras
 
 	}
-	sala(jugadores,sala,creador){ //sala de espera para entrar a partida (se llama cada vez que entra un jugador en la sala)
-		
-		////OCULTAMOS LOS ELEMENTOS DEL LOBBY
-		//document.getElementById("serpiente").style.display = "none";
-		//document.getElementById("ranking").style.display = "none";
-		////CREAMOS LOS ELEMENTOS DE LA SALA
-		//game.context.clearRect(0, 0, 900, 540);
-		var d = document.getElementById("salaActual");
-		borrarDiv('#salaActual');
-		this.context.font = "20px Tw Cen MT";
-		this.context.fillStyle = "white";
-		this.context.textAlign = "center";
-		salaP = sala;
-		this.context.fillText("Sala: " + sala, 300, 50);
-		var alto = 50;
-		var b1 = document.createElement("button");
-		b1.id = "salir";
-		b1.textContent = "Salir";
-
-		b1.addEventListener("click", salir); //boton de salir de la sala
-		
-		d.appendChild(b1);
-		if(jugadores.length >= 2 && name === creador){ //si hay 2 o mas jugadores, y el usuario es el creador de la sala, le aparece el boton de comenzar partida
-
-			var b2 = document.createElement("button");
-			b2.textContent = "Comenzar juego";
-			b2.id = "comenzar";
-			b2.addEventListener("click",function(){ //comienza partida
-
-				var ob = {
-
-					funcion: "comenzarPartida", //metodo invocado en Java
-					params: [salaP] //le pasamos el nombre de la sala para avisar a todos los jugadores que hay en ella
-
-				}
-
-				game.socket.send(JSON.stringify(ob));
-
-			})
-			d.appendChild(b2);
-
-		}
-
-		//MOSTRAMOS LOS JUGADORES QUE HAY EN LA SALA
-		var inc = 0;
-		for(var i = 0; i < jugadores.length; i++){
-
-			inc += 50;
-			this.context.font = "20px Tw Cen MT";
-			this.context.fillText(jugadores[i], 300, alto + inc);
-
-		}
-	
-	}
-
-    
 
 	connect() {
 
@@ -424,58 +309,6 @@ class Game {
 	}
 }
 
-function finEspera(){ //eliminamos el boton de cancelar y el mensaje
-
-	document.getElementById("cancelar").style.display = 'none';
-
-}
-function espera(){ //el usuario espera por si sale un jugador
-
-	borrarDiv('#cancelar');
-	var n = document.getElementById("cancelar");
-	n.style.display = 'inline-block';
-	n.innerHTML = "Intentando unirse \n a la sala...";
-	var boton = document.createElement("button");
-	boton.textContent = "Cancelar";
-	boton.id = "cancelarEspera";
-	boton.addEventListener("click", function(){ //si el usuario decide cancelar, se lo decimos al servidor
-
-		finEspera();
-		var ob = {
-
-			funcion: "cancelarEspera",
-			params: []
-
-		}
-
-		game.socket.send(JSON.stringify(ob));
-		
-		
-	});
-
-	n.appendChild(boton);
-
-}
-function addListaJugadores(jugadores){ //actualizamos la lista de jugadores online (se une un usuario)
-
-	$('#lista').empty();
-	document.getElementById("lista").textContent = "Jugadores Online";
-	for(var i = 0; i < jugadores.length; i++){
-		var j = document.createElement("div");
-		j.id = jugadores[i];
-		j.textContent = jugadores[i];
-		document.getElementById("lista").appendChild(j);
-	}
-
-}
-
-function removeListaJugadores(jugador){ //actualizamos la lista de jugadores online (se desconecta un usuario)
-
-	var n = document.getElementById(jugador);
-	document.getElementById("lista").removeChild(n);
-
-}
-
 function postPartida(d){ //post a APIRest para guardar la partida creada
 
 	document.getElementById("selector").style.display = 'none';
@@ -505,80 +338,6 @@ function postPartida(d){ //post a APIRest para guardar la partida creada
 	});
 
 }
-
-function buscar(dif){ //Matchmaking
-	
-	document.getElementById("selector").style.display = 'none';
-	var o = {
-		
-		funcion: "matchMaking",
-		params:[dif] //dificultad seleccionada por el usuario
-
-	}
-
-	game.socket.send(JSON.stringify(o));
-
-}
-function selector(funcion){ //selector de dificultad
-
-	document.getElementById("partidas-container").style.display = "none";
-	var dificultad; //facil:1,medio:2,dificil:4
-	var sel = document.getElementById("selector");
-	borrarDiv('#selector');
-	sel.style.display = 'inline-flex';
-	var p1 = document.createElement('p');
-	p1.innerHTML = "SELECCIONE UNA DIFICULTAD";
-	sel.appendChild(p1);
-	var p2 = document.createElement('p');
-	var facil = document.createElement('button');
-	facil.textContent = "FÁCIL";
-	facil.id = "f";
-	facil.addEventListener("click",function(){
-
-		dificultad = 1
-		if(funcion=="post")
-			postPartida(dificultad);
-		else
-			buscar(dificultad);
-
-		document.getElementById("partidas-container").style.display = "inline-block";
-	});
-	p2.appendChild(facil);
-	sel.appendChild(p2);
-	
-	var p3 = document.createElement('p');
-	var medio = document.createElement('button');
-	medio.textContent = "NORMAL";
-	medio.id = "m";
-	medio.addEventListener("click",function(){
-
-		dificultad = 2
-		if(funcion=="post")
-			postPartida(dificultad);
-		else
-			buscar(dificultad);
-		document.getElementById("partidas-container").style.display = "inline-block";
-	});
-	p3.appendChild(medio);
-	sel.appendChild(p3);
-	var p4 = document.createElement('p');
-	var dificil = document.createElement('button');
-	dificil.textContent = "DIFÍCIL";
-	dificil.id = "d";
-	dificil.addEventListener("click",function(){
-
-		dificultad = 4
-		if(funcion=="post")
-			postPartida(dificultad);
-		else
-			buscar(dificultad);
-		document.getElementById("partidas-container").style.display = "inline-block";
-	});
-	p4.appendChild(dificil);
-	sel.appendChild(p4);
-
-}
-
 
 $(document).ready(function(){
     $('#send-btn').click(function() { //boton de enviar del chat
@@ -625,14 +384,7 @@ window.onload = function(){
 	$('#playground').width(width);
 	$('#playground').height(height);
 
-	/*var img = new Image();
-	img.src = "../../resources/SPRITES/sprite1/sprite1.1.png";
-	img.onload= function(){
-		context.drawImage(img, 0,0, 110.4,205.2);
-	}*/
-
-	game  = new Game();
-	
+	game  = new Game();	
 	game.initialize();
 
 }
